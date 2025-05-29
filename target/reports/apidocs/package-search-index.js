33895e15a8f08a214291fd4c7662cd7540a1b9d3
Fatejian <hj20062007@gmail.com>
2025-05-29 13:49:47 +0800

    update some files

--- target/reports/apidocs/package-search-index.js
+++ target/reports/apidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.itranswarp.learnjava"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.itranswarp.rich"}];updateSearchResults();
--- target/reports/apidocs/type-search-index.js
+++ target/reports/apidocs/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.itranswarp.learnjava","l":"Main"}];updateSearchResults();
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"com.itranswarp.rich","l":"Millionaire"}];updateSearchResults();
